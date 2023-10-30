3551d24c59c2abea0465e22c4f4b83ea4c797d6f
jengis <jengis.d@gmail.com>
2023-10-31 03:29:24 +0600

    Add create_persons_table migration

---
+++ database/migrations/2023_10_30_133634_create_persons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->boolean('sex')->comment('false - female, true - male');
+            $table->date('birthdate')->nullable();
+            $table->string('notes')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->timestamps();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persons');
+    }
+};
